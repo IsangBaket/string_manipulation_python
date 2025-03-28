c00e5ced2377dd0ccf0e7fb01399b472b5756571
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:58:54 +0800

    fixed

--- name_to_pascal_casing.py
+++ name_to_pascal_casing.py
@@ -1,6 +1,8 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 name = input("Try typing your name in improper casing: ").lower()
-name = "".join(name.split()).title()
+name = name.title()         #allows program to capitalize initial letter of words before combining them
+name = "".join(name.split())
+
 
 print(name)
