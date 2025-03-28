f2ae12dd318471448d9358e8b2f536a939acf796
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:33:29 +0800

    first commit

---
+++ word_counter.py
@@ -0,0 +1,4 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+user_message = len(input("Enter a phrase or a sentence: ").split())
+print(user_message)
