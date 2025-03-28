f5603c13ba04248aba1b573fb689f8f07010fbb5
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:25:19 +0800

    fixed typo in line 3

--- casing_resolver.py
+++ casing_resolver.py
@@ -1,6 +1,6 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
-name = input("Try inputing your name in inproper casing: ").lower()
+name = input("Try inputing your name in improper casing: ").lower()
 name = name.title()
 
 print(f"Your name is {name}")
