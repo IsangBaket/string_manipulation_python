0b33a3c2a9f407b555d01d0707319bb2752a83ff
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:46:55 +0800

    removed unnecessary space in line 3

--- character_counter.py
+++ character_counter.py
@@ -1,4 +1,3 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input
 
-
 print(len(input("input your name: ").replace(" ","")))          #now stops program from counting spaces
