a208eb62e70beec0cf6c3a413c2fa6d2ca485472
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:56:45 +0800

    title method not working still looking for solutions

--- name_to_pascal_casing.py
+++ name_to_pascal_casing.py
@@ -1,6 +1,6 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 name = input("Try typing your name in improper casing: ").lower()
-name = name.join(name.split())
+name = "".join(name.split()).title()
 
-print(name.title())
+print(name)
