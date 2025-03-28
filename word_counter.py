23d99f7aa818619b7c99c921d0c0d1b5d6f4c035
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:35:40 +0800

    allowed the user input to still be displayed while still showing the word count

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,7 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
-user_message = len(input("Enter a phrase or a sentence: ").split())
+user_message = input("Enter a phrase or a sentence: ")
+word_count = len(user_message.split())
+
 print(user_message)
+print(word_count)
