3e1a67d445c532303ca3975f63e5cd123afa6519
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 21:24:36 +0800

    removed soace on line 4

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,5 +1,4 @@
 #Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 name = input("Try inputing your name in improper casing: ").swapcase()
-
 print(name)
