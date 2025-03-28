a5ca6d076bcbb62d6237b600f96f698b980076a3
IsangBaket? <max.bias@myyahoo.com>
2025-03-28 16:53:37 +0800

    made it to loop using functions

--- six_digit_format.py
+++ six_digit_format.py
@@ -1,6 +1,21 @@
 #Prog02: Create a program that ask the user to input a number (0-1000).
 #Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
-num = int(input("Input a number from 0 - 1000: "))
+def again():
+    user = input("would you like to try again? y or n: ").lower()
+    if user == "y":
+        digit()
+    else:
+        print("Thank you for trying out my simple program.")
 
-print(f"{num:06d}")
+
+def digit():
+    num = int(input("Input a number from 0 - 1000: "))
+    if num <= 1000:
+        print(f"{num:06d}")
+        again()
+    else:
+        print("The number you input is greater than 1000!")
+        again()
+
+digit()
